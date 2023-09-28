d96e83efd5ae246ffbeec4c76e13fc1ee0ce010b
Elmoris5 <mohamedelmorse5@gmail.com>
2023-09-28 07:27:37 +0300

    Executing Modules as Scripts

--- 08 Modules/sales.py
+++ 08 Modules/sales.py
@@ -4,3 +4,8 @@ def calc_tax():
 
 def calc_shipping():
     print("Shipping")
+
+
+if __name__ == '__main__':
+    print('this line of code will be only executed only when we run this module directly not importing')
+    print('in simply way, we use it to prevent some block of code to be executed when importing this module')
